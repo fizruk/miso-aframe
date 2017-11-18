f8f4984ed3d3756388085372b23a0f48b264d891
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2017-11-18 17:57:04 +0300

    Add reloadAFrameApp helper for GHCJSi

--- src/Miso/AFrame.hs
+++ src/Miso/AFrame.hs
@@ -10,6 +10,8 @@ module Miso.AFrame (
   entity,
   scene,
 
+  reloadAFrameApp,
+
 ) where
 
 import Miso
@@ -21,8 +23,35 @@ import Miso.AFrame.Primitives.Camera
 import Miso.AFrame.Primitives.Cursor
 import Miso.AFrame.Primitives.Light
 
+-- | An entity.
 entity :: [Attribute action] -> [View action] -> View action
 entity = node HTML "a-entity" Nothing
 
 scene :: [Attribute action] -> [View action] -> View action
 scene = node HTML "a-scene" Nothing
+
+foreign import javascript unsafe
+  "(function(){ document.body.innerHTML = ''; })();"
+  clearBody :: IO ()
+
+foreign import javascript unsafe
+  "(function () { \
+  \  var s = document.createElement('script'); \
+  \  s.type = 'text/javascript'; \
+  \  s.async = true; \
+  \  s.src = $1; \
+  \  var x = document.getElementsByTagName('script')[0]; \
+  \    x.parentNode.insertBefore(s, x); \
+  \  })(); "
+  loadScriptAsync :: MisoString -> IO ()
+
+-- | (Re)load an AFrame app from GHCJSi.
+--
+-- This will load AFrame from
+-- <https://aframe.io/releases/0.6.1/aframe.min.js>.
+reloadAFrameApp :: IO () -> IO ()
+reloadAFrameApp app = do
+  clearBody
+  loadScriptAsync "https://aframe.io/releases/0.6.1/aframe.min.js"
+  app
+
