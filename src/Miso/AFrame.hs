7e973a31c33071e5133c99b61df005c99f02f67f
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2017-12-01 18:10:35 +0300

    Stop reloading aframe JS when reloading app

--- src/Miso/AFrame.hs
+++ src/Miso/AFrame.hs
@@ -27,6 +27,7 @@ import Miso.AFrame.Primitives.Light
 entity :: [Attribute action] -> [View action] -> View action
 entity = node HTML "a-entity" Nothing
 
+-- | A-Frame scene.
 scene :: [Attribute action] -> [View action] -> View action
 scene = node HTML "a-scene" Nothing
 
@@ -34,24 +35,11 @@ foreign import javascript unsafe
   "(function(){ document.body.innerHTML = ''; })();"
   clearBody :: IO ()
 
-foreign import javascript unsafe
-  "(function () { \
-  \  var s = document.createElement('script'); \
-  \  s.type = 'text/javascript'; \
-  \  s.async = true; \
-  \  s.src = $1; \
-  \  var x = document.getElementsByTagName('script')[0]; \
-  \    x.parentNode.insertBefore(s, x); \
-  \  })(); "
-  loadScriptAsync :: MisoString -> IO ()
-
--- | (Re)load an AFrame app from GHCJSi.
+-- | (Re)load GHCJS app from GHCJSi.
 --
--- This will load AFrame from
--- <https://aframe.io/releases/0.6.1/aframe.min.js>.
+-- This will clear <body> and run an app.
 reloadAFrameApp :: IO () -> IO ()
 reloadAFrameApp app = do
   clearBody
-  loadScriptAsync "https://aframe.io/releases/0.6.1/aframe.min.js"
   app
 
