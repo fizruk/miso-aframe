0e6cb1f34518ca5fc44b20e9b7ac1fa07c2b70a6
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-05-06 23:36:13 +0300

    Add Docker support
    Now you can easily build the project using Docker image
    that contains everything you'll need:
    
    - GHCJS compiler already built
    - GHCJSi patched to support static file serving

--- repl.sh
+++ repl.sh
@@ -5,18 +5,15 @@ set -x
 
 # example project name
 name=$1
+shift
 
-# always run from a directory this script is in
-cd $(dirname $0)
-
-# find out where GHCJS /lib/etc files are located
-GHCJS_LIB_ETC_PATH=$(dirname $(stack path --global-pkg-db))
-
-# replace irunner.js to enable static file server
-cp ghcjs/lib/etc/irunner.js ${GHCJS_LIB_ETC_PATH}/irunner.js
+# optional stack options
+STACK_OPTIONS=$@
 
 # specify a directory to serve static files from
 export GHCJSI_STATIC_DIR=$(pwd)/examples/$name/static
 
 # run GHCJSi
-stack ghci miso-aframe:lib miso-aframe:exe:$name
+stack ghci miso-aframe:lib miso-aframe:exe:$name $STACK_OPTIONS \
+  --docker-mount "${GHCJSI_STATIC_DIR}" \
+  --docker-env "GHCJSI_STATIC_DIR=${GHCJSI_STATIC_DIR}"
